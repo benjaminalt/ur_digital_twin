8c31209447c911861014be6be191f5445858d3be
Benjamin Alt <benjamin_alt@outlook.com>
2019-11-05 22:06:43 +0100

    Gazebo UR mirrors real UR

--- scripts/joint_state_publisher.py
+++ scripts/joint_state_publisher.py
@@ -3,7 +3,8 @@
 import rospy
 from std_msgs.msg import Float64MultiArray
 from controller_manager_msgs.srv import SwitchController, SwitchControllerRequest, ListControllers, ListControllersRequest
-import math
+from ur_joint_state_reader import URJointStateReader
+
 
 def switch_to_position_controller():
     rospy.wait_for_service("/controller_manager/list_controllers")
@@ -24,48 +25,28 @@ def switch_to_position_controller():
     req.strictness = 2
     switch_controller(req)
 
-def base_configuration():
-    state = Float64MultiArray()
-    state.data = [0.0, -2.0897, -1.6473, -2.5590, 3.6376, 5.3446]
-    return state
-
-def generate_joint_states(interval):
-    state = base_configuration()
-    vel = 1.0 # rad/s
-    step = vel * interval # rad
-    limit_min = - math.pi
-    limit_max = math.pi
-    increment = True
-    while True:
-        if increment:
-            if state.data[0] + step > limit_max:
-                increment = False
-                continue
-            state.data[0] += step
-        else:
-            if state.data[0] - step < limit_min:
-                increment = True
-                continue
-            state.data[0] -= step
-        yield state
-
 
 def main():
     node_name = "joint_state_publisher"
     rospy.loginfo("{} starting".format(node_name))
     rospy.init_node(node_name, anonymous=False)
 
+    config_file = rospy.get_param("ursim_rtde_config")
+    host = rospy.get_param("ursim_host")
+    port = rospy.get_param("ursim_port")
+    sampling_interval = rospy.get_param("sampling_interval")
+    rate = rospy.Rate(1/sampling_interval)
+
     try:
         switch_to_position_controller()
-
         pub = rospy.Publisher("/joint_group_position_controller/command", Float64MultiArray, queue_size=10)
-        interval = 0.02
-        rate = rospy.Rate(1/interval)
-        for state in generate_joint_states(interval):
-            if rospy.is_shutdown():
-                break
-            pub.publish(state)
-            rate.sleep()
+        with URJointStateReader(config_file, host, port, sampling_interval) as joint_state_reader:
+            while not rospy.is_shutdown():
+                current_state = joint_state_reader.receive(False)
+                state = Float64MultiArray()
+                state.data = current_state.actual_q
+                pub.publish(state)
+                rate.sleep()
     except rospy.exceptions.ROSInterruptException:
         return
 
---
+++ scripts/ur_joint_state_reader.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+from rtde import rtde
+import rtde.rtde_config as rtde_config
+
+class URJointStateReader(object):
+    def __init__(self, config_file, host, port, sampling_interval=0.008):
+        self.config_file = config_file
+        self.host = host
+        self.port = port
+        self.sampling_interval = sampling_interval
+        self.connection = None
+
+    def __enter__(self):
+        conf = rtde_config.ConfigFile(self.config_file)
+        output_names, output_types = conf.get_recipe('out')
+        self.connection = rtde.RTDE(self.host, self.port)
+        self.connection.connect()
+        if not self.connection.send_output_setup(output_names, output_types, frequency = 1/self.sampling_interval):
+            raise RuntimeError('Unable to configure output')
+        if not self.connection.send_start():
+            raise RuntimeError('Unable to start synchronization')
+        return self.connection
+
+    def __exit__(self, *args):
+        self.connection.send_pause()
+        self.connection.disconnect()
